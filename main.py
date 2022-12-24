dab5425515c542ccb2d9c365c177665e32026d7f
Hargun Sandhu <imhargunsandhu@gmail.com>
2022-12-24 20:00:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#Variables Start
+num = int(input("Enter Your Number For Table: "))
+tab = 1
+i = 1
+#Variables End
+#Loop Start
+while i<=15:
+    tab=num*i
+    print(num, "x", i, "=", tab)
+    i=i+1
+#Loop End
